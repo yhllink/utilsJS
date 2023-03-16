2ed440777e17af0e44bddc1f9652ace9ff3994bc
姚翰林 <yaohlin@yonyou.com>
2023-03-16 14:19:56 +0800

    修改

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import Again from './Again/Again'
+import IndexedDB from './IndexedDB/IndexedDB'
 import JSONParse from './JSONParse/JSONParse'
 import encodeURIZnCh from './encodeURIZnCh/encodeURIZnCh'
 import getOffset from './getOffset/getOffset'
 import handleUrl from './handleUrl/handleUrl'
 import ifType from './ifType/ifType'
-import indexedDB from './indexedDB/indexedDB'
 import isType from './isType/isType'
 import loadModules from './loadModules/loadModules'
 import nextTick from './nextTick/nextTick'
 import prefixInteger from './prefixInteger/prefixInteger'
 import structure from './structure/structure'
 
-export { Again, JSONParse, encodeURIZnCh, getOffset, handleUrl, ifType, indexedDB, isType, loadModules, nextTick, prefixInteger, structure }
+export { Again, IndexedDB, JSONParse, encodeURIZnCh, getOffset, handleUrl, ifType, isType, loadModules, nextTick, prefixInteger, structure }
