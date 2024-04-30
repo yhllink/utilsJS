5029654c4d1764696f10940f9e95416d7846089c
姚翰林 <yaohlin@yonyou.com>
2024-04-30 10:24:48 +0800

    通信由 BroadcastChannel改为addEventListener('storage'

--- src/sessionShareStorage/sessionShareStorage.ts
+++ src/sessionShareStorage/sessionShareStorage.ts
@@ -13,63 +13,78 @@ const sessionShareStorage = (function () {
     } as Storage)
   }
 
+  const sessionShareChannelKey = '_sessionShareChannelKey_4gv121d12'
   const shareName = 'share-'
   const pageRid = 'rid-' + Math.floor(Math.random() * 100000000)
-  const channel = new BroadcastChannel('sessionShareStorage')
+  const getKey = function (key: string) {
+    return shareName + key
+  }
+
+  const channel = function (data: any) {
+    window.localStorage.setItem(sessionShareChannelKey, JSON.stringify(data))
+  }
 
   const sessionShareStorage: Storage = Object.freeze({
     key(index: number) {
       return window.sessionStorage.key(index)
     },
     getItem(key: string) {
-      return window.sessionStorage.getItem(shareName + key)
+      return window.sessionStorage.getItem(getKey(key))
     },
     get length() {
       return window.sessionStorage.length
     },
 
     setItem(key: string, value: string) {
-      channel.postMessage({ type: 'setItem', key: key, value: value })
-      return window.sessionStorage.setItem(shareName + key, value)
+      channel({ type: 'setItem', key: key, value: value })
+      return window.sessionStorage.setItem(getKey(key), value)
     },
     removeItem(key: string) {
-      channel.postMessage({ type: 'removeItem', key: key })
-      return window.sessionStorage.removeItem(shareName + key)
+      channel({ type: 'removeItem', key: key })
+      return window.sessionStorage.removeItem(getKey(key))
     },
     clear() {
-      channel.postMessage({ type: 'clear' })
+      channel({ type: 'clear' })
       return window.sessionStorage.clear()
     },
     getOtherPageStore() {
-      channel.postMessage({ type: 'getAll', rid: pageRid })
+      channel({ type: 'getAll', rid: pageRid })
     },
   })
 
-  channel.onmessage = function (e) {
-    const data = e.data
+  window.addEventListener('storage', function (e) {
+    if (e.key !== sessionShareChannelKey) return
+    if (!e.newValue) return
 
-    if (data?.type === 'setItem') window.sessionStorage.setItem(shareName + data.key, data.value)
-    if (data?.type === 'removeItem') window.sessionStorage.removeItem(shareName + data.key)
-    if (data?.type === 'clear') window.sessionStorage.clear()
+    const data = JSON.parse(e.newValue)
+    if (!data?.type) return
 
-    if (data?.type === 'getAll' && data?.rid && data.rid !== pageRid) {
-      channel.postMessage({
-        type: 'setAll',
-        rid: data.rid,
-        data: JSON.parse(
-          JSON.stringify(window.sessionStorage, function (key, val) {
-            if (key && key.indexOf(shareName) !== 0) return undefined
-            return val
-          }) ?? '{}'
-        ),
-      })
+    if (data.type === 'setItem') window.sessionStorage.setItem(getKey(data.key), data.value)
+    if (data.type === 'removeItem') window.sessionStorage.removeItem(getKey(data.key))
+    if (data.type === 'clear') window.sessionStorage.clear()
+
+    if (data.type === 'getAll' && data.rid !== pageRid) {
+      const dataStr =
+        JSON.stringify(window.sessionStorage, function (key, val) {
+          if (key && key.indexOf(shareName) !== 0) return undefined
+          return val
+        }) ?? '{}'
+      if (dataStr !== '{}') {
+        channel({
+          type: 'setAll',
+          rid: data.rid,
+          data: JSON.parse(dataStr),
+        })
+      }
     }
-    if (data?.type === 'setAll' && data?.rid === pageRid) {
+    if (data.type === 'setAll' && data.rid === pageRid && typeof data.data === 'object') {
       for (const key in data.data) {
         if (key.indexOf(shareName) === 0) window.sessionStorage.setItem(key, data.data[key])
       }
     }
-  }
+
+    window.localStorage.removeItem(sessionShareChannelKey)
+  })
 
   sessionShareStorage.getOtherPageStore()
   return sessionShareStorage
