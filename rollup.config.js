87908d764c803be18ada6ec7090799b13d6a351b
姚翰林 <yaohlin@yonyou.com>
2024-08-20 13:26:23 +0800

    暂存，等待优化

--- rollup.config.js
+++ rollup.config.js
@@ -7,25 +7,6 @@ import pkg from './package.json'
 deletePathSync(path.resolve(pkg.main, '../'))
 deletePathSync(path.resolve(pkg.module, '../'))
 
-function getRollupConfig({ input, output }) {
-  return {
-    input,
-    output: [
-      { file: path.resolve(pkg.main, ...output), format: 'cjs', exports: 'auto' },
-      { file: path.resolve(pkg.module, ...output), format: 'esm', exports: 'auto' },
-    ],
-    plugins: [
-      typescript({
-        verbosity: 0,
-        useTsconfigDeclarationDir: true,
-        tsconfig: path.resolve(__dirname, 'tsconfig.json'),
-        tsconfigOverride: { compilerOptions: { target: 'es5' } },
-      }),
-    ],
-    external: (id) => !/^(src\/.*)/.test(id) && !id.startsWith('.'),
-  }
-}
-
 const srcPath = path.resolve(__dirname, './src')
 const inputPath = path.resolve(srcPath, './index.ts')
 
@@ -38,12 +19,22 @@ let indexStr = ''
 for (let i = 0, l = srcList.length; i < l; i++) {
   const name = srcList[i]
   indexStr += `import ${name} from './${name}/${name}'\n`
-  utilsConfigs.push(
-    getRollupConfig({
-      input: path.resolve(srcPath, name, name + '.ts'),
-      output: () => ['../', name, 'index.js'],
-    })
-  )
+  utilsConfigs.push({
+    input: path.resolve(srcPath, name, name + '.ts'),
+    output: [
+      { file: path.resolve(pkg.main, '../', name, 'index.js'), format: 'cjs', exports: 'auto' },
+      { file: path.resolve(pkg.module, '../', name, 'index.js'), format: 'esm', exports: 'auto' },
+    ],
+    plugins: [
+      typescript({
+        verbosity: 0,
+        useTsconfigDeclarationDir: true,
+        tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+        cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache'),
+        tsconfigOverride: { compilerOptions: { target: 'es5' } },
+      }),
+    ],
+  })
 }
 indexStr += `\nexport { ${srcList.join(', ')} }`
 writeFileSync(inputPath, indexStr)
@@ -52,12 +43,39 @@ const { data: lazyList } = readDirSync(path.resolve(__dirname, './src/lazy'))
 for (let i = 0, l = lazyList.length; i < l; i++) {
   const name = lazyList[i]
 
-  utilsConfigs.push(
-    getRollupConfig({
-      input: path.resolve(srcPath, 'lazy', name, name + '.ts'),
-      output: () => ['../', 'lazy', name, 'index.js'],
-    })
-  )
+  utilsConfigs.push({
+    input: path.resolve(srcPath, 'lazy', name, name + '.ts'),
+    output: [
+      { file: path.resolve(pkg.main, '../', 'lazy', name, 'index.js'), format: 'cjs', exports: 'auto' },
+      { file: path.resolve(pkg.module, '../', 'lazy', name, 'index.js'), format: 'esm', exports: 'auto' },
+    ],
+    plugins: [
+      typescript({
+        verbosity: 0,
+        useTsconfigDeclarationDir: true,
+        tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+        cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache'),
+        tsconfigOverride: { compilerOptions: { target: 'es5' } },
+      }),
+    ],
+  })
 }
 
-export default [getRollupConfig({ input: inputPath, output: () => [] }), ...utilsConfigs]
+export default [
+  {
+    input: inputPath,
+    output: [
+      { file: pkg.main, format: 'cjs', exports: 'auto' },
+      { file: pkg.module, format: 'esm', exports: 'auto' },
+    ],
+    plugins: [
+      typescript({
+        verbosity: 0,
+        tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+        cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache'),
+        tsconfigOverride: { compilerOptions: { target: 'es5' }, include: ['src'] },
+      }),
+    ],
+  },
+  ...utilsConfigs,
+]
