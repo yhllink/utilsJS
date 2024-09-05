c02d59c051805946fc182bc5f3e27157a130b1a2
姚翰林 <yaohlin@yonyou.com>
2024-09-05 19:30:59 +0800

    优化

--- src/formatAmount/formatAmount.ts
+++ src/formatAmount/formatAmount.ts
@@ -5,41 +5,44 @@ import { formatNumber } from '@/formatNumber/formatNumber'
  * @param {string | number} money 金额
  * @returns {string} 处理后的金额
  */
-export function formatAmount(
+function formatAmount(
   money: string | number,
-  options?: {
-    auto?: boolean
-    max?: number
-    maxStr?: string
-    decimalPlaces?: number
-    module?: boolean
-  }
-) {
+  options?: { auto?: boolean; max?: number; maxStr?: string; decimalPlaces?: number; accuracy?: number; module?: false }
+): string
+function formatAmount(
+  money: string | number,
+  options?: { auto?: boolean; max?: number; maxStr?: string; decimalPlaces?: number; accuracy?: number; module?: true }
+): [string, string]
+function formatAmount(
+  money: string | number,
+  options?: { auto?: boolean; max?: number; maxStr?: string; decimalPlaces?: number; accuracy?: number; module?: boolean }
+): string | [string, string] {
   // 默认参数设置
-  const { auto = true, max = 10000, maxStr = '万', decimalPlaces = 2, module = false } = options || {}
+  const { auto = true, max = 10000, maxStr = '万', decimalPlaces = 2, accuracy = 2, module = false } = options || {}
 
   // 将金额转换为数字类型
   const numMoney = Number(money)
 
   // 根据auto参数值，动态设置金额上限和单位
   const [endMax, endStr] = (() => {
-    if (auto) {
+    if (!auto) {
       if (numMoney >= 100000000) return [100000000, '亿']
       if (numMoney >= 10000000) return [10000, '万']
     }
-
     return [max, maxStr]
   })()
 
   // 如果金额小于1万，直接格式化并返回
   if (numMoney < 10000) {
-    const amount = formatNumber(numMoney, decimalPlaces)
+    const amount = String(formatNumber(numMoney, decimalPlaces, accuracy))
     if (module) return [amount, '']
     return amount
   }
 
   // 格式化金额并加上单位，根据module参数值返回不同格式的结果
-  const amount = formatNumber(numMoney / endMax, decimalPlaces)
+  const amount = String(formatNumber(numMoney / endMax, decimalPlaces, accuracy))
   if (module) return [amount, endStr]
   return amount + endStr
 }
+
+export { formatAmount }
--- src/formatNumber/formatNumber.ts
+++ src/formatNumber/formatNumber.ts
@@ -8,15 +8,21 @@
  * @param decimalPlaces 保留的小数位数，默认为2
  * @returns 格式化后的数值字符串，如果输入值无效则原样返回
  */
-export function formatNumber(value: string | number, decimalPlaces: number = 2) {
+export function formatNumber(value: string | number, decimalPlaces: number = 2, accuracy: number = 2) {
   // 将输入值转换为浮点数，以便后续处理
   const numValue = parseFloat(String(value))
   // 检查转换后的值是否为有效的数字，如果不是则返回原始值
   if (typeof numValue !== 'number' || isNaN(numValue)) return value
 
   // 使用toLocaleString方法格式化数字，确保小数位数符合要求
-  return numValue.toLocaleString(undefined, {
+  const numStr = numValue.toLocaleString(undefined, {
     minimumFractionDigits: decimalPlaces,
     maximumFractionDigits: decimalPlaces,
   })
+
+  const [integer, decimal] = numStr.split('.')
+  const decimalArr = decimal.split('')
+  if (decimalArr.length > accuracy) decimalArr.length = accuracy
+
+  return [integer, decimalArr.join('')].join('.')
 }
